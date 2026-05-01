8cec6d657323a8be84ace1608af61a6179224123
Kamil Marszałek <kamilmar553@gmail.com>
2026-05-01 19:04:08 +0200

    feat: add candles fetching to main screen

--- src/commonMain/kotlin/App.kt
+++ src/commonMain/kotlin/App.kt
@@ -7,36 +7,41 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import data.ApiResult
+import data.Candle
 import data.StockProvider
 import data.StockQuote
 import data.createHttpClient
+import indicators.simpleMovingAverage
 
 @Composable
 fun App() {
     val stockProvider = remember { StockProvider(createHttpClient(), AppConfig.API_KEY) }
 
     // Immutable state (on new state old one is overwritten)
-    var state by remember { mutableStateOf<ApiResult<StockQuote>?>(null) }
+    var state by remember { mutableStateOf<ApiResult<List<Candle>>?>(null) }
 
     // Side effect
     LaunchedEffect(Unit) {
-        state = stockProvider.fetchQuote("NVDA")
+        state = stockProvider.fetchCandles("AAPL")
     }
 
     MaterialTheme {
         Column(modifier = Modifier.padding(16.dp)) {
             when (val current = state) {
-                null -> Text("Downloading data...")
+                null -> Text("Downloading candles...")
                 is ApiResult.Failure -> {
                     Text("Error: ${current.message}", color = Color.Red)
                     println(current.message)
                 }
                 is ApiResult.Success -> {
-                    val stock = current.data
-                    Text("Ticker: ${stock.symbol}", style = MaterialTheme.typography.h5)
-                    Text("Price: ${stock.price}")
-                    Text("Change percentage: ${stock.changePercentage}%",
-                        color = if (stock.changePercentage >= 0) Color.Green else Color.Red)
+                    val candles = current.data
+                    val closes = candles.map {it.close}
+                    val sma20 = simpleMovingAverage(closes, 20)
+                    Text("Candles downloaded: ${candles.size}")
+                    Text("First candle date: ${candles.firstOrNull()?.date}")
+                    Text("Latest close: ${candles.firstOrNull()?.close}")
+                    Text("Latest SMA20: ${sma20.firstOrNull() {it != null}}")
+
                 }
             }
         }
