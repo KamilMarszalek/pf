1dcb2d3b83c0b4c8383fc5fe89abe5befc63a8ec
Kamil Marszałek <kamilmar553@gmail.com>
2026-05-02 10:17:31 +0200

    fix: delete unused import

--- src/commonMain/kotlin/data/StockProvider.kt
+++ src/commonMain/kotlin/data/StockProvider.kt
@@ -1,6 +1,5 @@
 package data
 import io.ktor.client.*
-import io.ktor.client.call.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
