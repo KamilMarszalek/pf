ae86dc6aecae8667b2f04988e543d4d8e348b342
Kamil Marszałek <kamilmar553@gmail.com>
2026-05-29 23:31:14 +0200

    fix: get back to arrows

--- src/commonMain/kotlin/ui/CandlestickChart.kt
+++ src/commonMain/kotlin/ui/CandlestickChart.kt
@@ -160,7 +160,7 @@ fun CandlestickChart(
 
                 val isPositive = percentageChange >= 0
                 val badgeColor = if (isPositive) Color(0xFF26A69A) else Color(0xFFEF5350)
-                val sign = if (isPositive) "up +" else "down "
+                val sign = if (isPositive) "▲ +" else "▼ "
 
                 Card(
                     backgroundColor = badgeColor.copy(alpha = 0.9f),
